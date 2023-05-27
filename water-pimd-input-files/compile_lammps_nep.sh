bd16db365760af055d116790608dd10813ae56b2
zkunchen <zkuchen@ucdavis.edu>
2023-05-27 09:17:53 +0800

    Update compile_lammps_nep.sh for lmp_mpi

--- water-pimd-input-files/compile_lammps_nep.sh
+++ water-pimd-input-files/compile_lammps_nep.sh
@@ -12,11 +12,11 @@ curdir=$(pwd)
 cp $NEP_CPU_PATH/src/nep.*  $NEP_CPU_PATH/interface/lammps/USER-NEP/
 cp -r $NEP_CPU_PATH/interface/lammps/USER-NEP/ $LAMMPS_PATH/src
 cd $LAMMPS_PATH/src
-make clean-serial
+make clean-mpi
 make no-user-nep
 make yes-MISC
 make yes-extra-compute
 make yes-extra-dump
 make yes-user-nep
-make serial -j 8
-mv lmp_serial $curdir
+make mpi -j 8
+mv lmp_mpi $curdir
